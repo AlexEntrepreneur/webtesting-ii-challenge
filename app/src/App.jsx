f8b5ee6d05a9e4f5f355fc6c1256eabf2f75e185
Alex Entrepreneur <hello@alexentrepreneur.com>
2019-04-09 15:49:40 +0100

    Create Components Structure

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,9 +1,21 @@
 import React, { Component } from 'react';
+import Display from "./Components/Display";
+import Dashboard from "./Components/Dashboard";
 
 class App extends Component {
+  state = {
+    balls: 0,
+    pitches: 0
+  };
+
   render() {
     return (
       <div className="App">
+        <Display
+          balls={this.state.balls}
+          pitches={this.state.pitches}
+        />
+        <Dashboard />
       </div>
     );
   }
