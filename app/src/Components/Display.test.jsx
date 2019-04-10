8b9e7d06c8e5d8b3605440321b695e4216996d63
Alex Entrepreneur <hello@alexentrepreneur.com>
2019-04-10 23:42:05 +0100

    Edit Display Tests

--- app/src/Components/Display.test.jsx
+++ app/src/Components/Display.test.jsx
@@ -1,26 +1,26 @@
-import React from "react";
-import * as rtl from "react-testing-library";
-import Display from "./Display";
+import React from 'react';
+import * as rtl from 'react-testing-library';
+import Display from './Display';
 
 afterEach(rtl.cleanup);
 
-describe("Display", () => {
-  it("should display balls and strikes", () => {
+describe('Display', () => {
+  it('should display balls and strikes elements', () => {
     const displayComponent = rtl.render(<Display balls={0} strikes={0} />);
-    expect(displayComponent.getByText(/balls/i));
-    expect(displayComponent.getByText(/strikes/i));
+    expect(displayComponent.getByText(/Balls/g));
+    expect(displayComponent.getByText(/Strikes/g));
   });
-  it("should display the number of balls", () => {
+  it('should display the number of balls', () => {
     const displayComponent = rtl.render(<Display balls={2} strikes={0} />);
-    expect(displayComponent.getByText(/balls: 2/i));
+    expect(displayComponent.getByText(/Balls: 2/g));
   });
-  it("should display the number of strikes", () => {
+  it('should display the number of strikes', () => {
     const displayComponent = rtl.render(<Display balls={0} strikes={2} />);
-    expect(displayComponent.getByText(/strikes: 2/i));
+    expect(displayComponent.getByText(/Strikes: 2/g));
   });
-  it("should display 0 when no props are given", () => {
+  it('should display 0 when no props are given', () => {
     const displayComponent = rtl.render(<Display />);
-    expect(displayComponent.getByText(/strikes: 0/i));
-    expect(displayComponent.getByText(/balls: 0/i));
+    expect(displayComponent.getByText(/Strikes: 0/g));
+    expect(displayComponent.getByText(/Balls: 0/g));
   });
 });
